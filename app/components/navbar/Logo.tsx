ac4869bbce50b30f2e37bf8e094725e50f9addb7
ShahZaib-011 <muhammadshahzaib706@gmail.com>
2023-08-01 20:47:01 +0500

    "Structure build up"

---
+++ app/components/navbar/Logo.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+import { useRouter } from 'next/navigation'
+import Image from 'next/image'
+
+const Logo = () => {
+    const router = useRouter()
+    return  (
+        <>
+        <Image className='hidden md:block cursor-pointer' src="/images/logo.png" alt="logo" width="100" height="100"/>
+        </>
+    )
+}
+
+export default Logo
